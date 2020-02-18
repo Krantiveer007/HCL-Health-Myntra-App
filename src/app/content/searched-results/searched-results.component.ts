af9326f302201c47366d56a8ebf7a71af2a1137c
Krantiveer007 <34069776+Krantiveer007@users.noreply.github.com>
2020-02-18 21:55:50 +0530

    Add files via upload

---
+++ src/app/content/searched-results/searched-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searched-results',
+  templateUrl: './searched-results.component.html',
+  styleUrls: ['./searched-results.component.css']
+})
+export class SearchedResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
